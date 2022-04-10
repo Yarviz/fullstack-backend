37dfee4820dbe72eac23ecf34aa9ba1a7f757b87
Yarviz <juzzzzi@hotmail.com>
2022-04-10 11:34:56 +0300

    Exercises 3.13 and 3.14 done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-var bodyparser = require('body-parser')
+const bodyparser = require('body-parser')
 const userdata = require('./userdata.json');
 const cors = require('cors')
+const Person = require('./models/person');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 const app = express();
 const ID_RANGE = 100000000;
 
@@ -21,8 +22,12 @@ app.use(bodyparser.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 
 app.get('/api/persons', (req, res) => {
-    console.log(userdata);
-    res.status(200).send(userdata);
+    Person.find({}).then(result => {
+        console.log(result)
+        const all_persons = result.map(p => p.toJSON())
+        console.log(all_persons);
+        res.status(200).send(all_persons);
+    })
 })
 
 app.post('/api/persons', (req, res) => {
@@ -32,11 +37,14 @@ app.post('/api/persons', (req, res) => {
     if (!name || !number) {
         return res.status(400).send(`required params name and number in request`);
     }
-    if (userdata.find(obj => obj.name == name)) {
-        return res.status(409).send(`name must be unique`);
-    }
-    userdata.push({name, number, id: Math.floor(Math.random() * ID_RANGE)})
-    res.status(200).json(userdata[userdata.length - 1]);
+    //if (userdata.find(obj => obj.name == name)) {
+    //    return res.status(409).send(`name must be unique`);
+    //}
+    //userdata.push({name, number, id: Math.floor(Math.random() * ID_RANGE)})
+    const new_person = new Person({name, number});
+    new_person.save().then(result => {
+        res.status(200).json(result);
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
